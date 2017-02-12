7119a2e20d74815567f0023109f1031dfa3bdaeb
arno-di-loreto <arno.di.loreto@gmail.com>
2017-02-12 19:29:57 +0100

    Fix codehighlight.rb which was not using codeblocksize in _config.yml

--- _plugins/codehighlight.rb
+++ _plugins/codehighlight.rb
@@ -33,10 +33,17 @@ module Jekyll
 
     end
 
+    def lookup(context, name)
+      lookup = context
+      name.split(".").each { |value| lookup = lookup[value] }
+      lookup
+    end
+
     def render(context)
       code = h(super).strip
       
-      if code.lines.count > 20 
+      codeblocksize = lookup(context, 'site.codeblocksize')
+      if code.lines.count > codeblocksize 
         collapsed = " code-collapsed"
         collabpedbutton = "<button type=\"button\" class=\"btn btn-default\" onclick=\"toggle(this, this.parentElement.parentElement.parentElement.children[1], this.parentElement.parentElement.parentElement.children[2].children[0])\"><i class=\"fa fa-expand\" aria-hidden=\"true\"></i></button>"
         collapsedbottombutton = "<div class=\"code-bottom-toolbar\"><button type=\"button\" class=\"btn btn-default btn-block\" onclick=\"toggle(this, this.parentElement.parentElement.children[1], this.parentElement.parentElement.children[0].children[0].children[0], true)\"><i class=\"fa fa-expand\" aria-hidden=\"true\"></i></button></div>"
