784545502062e4f19753b3267bdedc669977eed3
arno-di-loreto <arno.di.loreto@gmail.com>
2017-02-12 11:18:27 +0100

    Added permalink and custom code folder handling do codefilehighlight plugin

--- _plugins/codefilehighlight.rb
+++ _plugins/codefilehighlight.rb
@@ -6,12 +6,6 @@ class CodeFile < Liquid::Tag
     markup.scan(::Liquid::TagAttributes) do |key, value|
       @attributes[key] = value.gsub("\"", "")
     end
-    
-    if @attributes['language']
-      @language =  @attributes['language']
-    else
-      raise SyntaxError.new("Syntax Error in 'code' - language is mandatory")
-    end
 
     if @attributes['numbers'] == "false"
       @linenumbers = ""
@@ -25,52 +19,83 @@ class CodeFile < Liquid::Tag
       @highlight = ""
     end
 
-    if @attributes['filename']
-      @filename =  @attributes['filename']
+    if @attributes['file']
+      @filename =  @attributes['file']
     else
-      raise SyntaxError.new("Syntax Error in 'codefile' - filename is mandatory")
+      raise SyntaxError.new("Syntax Error in 'codefile' - file is mandatory")
     end
-    @path = File.join('code', @filename);
-    @url = "/" + @path
-    @id = @path + "-" + rand(1000).to_s
-    @id = @id.gsub("/", "-").gsub(".","-")
-    if File.exists?(@path)
-      if @attributes['lines']
-        lines = File.readlines(@path)
+
+    if @attributes['language']
+      @language =  @attributes['language']
+    else
+      @language = @filename.split(".").last
+      if ! @language 
+        raise SyntaxError.new("Syntax Error in 'code' - language or file extension is mandatory")
+      end
+    end
+    
+    if @attributes['lines']
+      @linerange = @attributes['lines']
+    end
+
+  end
+
+  def lookup(context, name)
+    lookup = context
+    name.split(".").each { |value| lookup = lookup[value] }
+    lookup
+  end
+
+  def render(context)
+    coderoot = context.registers[:site].config['coderoot']
+    if @filename.start_with?(coderoot)
+      path = @filename
+    else
+      customcodefolder = lookup(context, 'page.codefiles')
+      if customcodefolder
+        codefolder = customcodefolder
+      else
+        codefolder = lookup(context, 'page.permalink')
+      end
+      path = File.join(coderoot, codefolder, @filename)
+    end
+    baseurl = context.registers[:site].config['baseurl']
+    url = "#{baseurl}/#{path}"
+    if File.exists?(path)
+      if @linerange
+        lines = File.readlines(path)
         # lines can be a single line "3" or a range "3-6"
-        params = @attributes['lines'].split('-')
+        params = @linerange.split('-')
         first = params[0].to_i
         if params[1]
           last = params[1].to_i
         else
           last = first
         end
-        @code = ""
+        code = ""
         for index in first-1 ... last
-          @code = @code + lines[index]
+          code = code + lines[index]
         end
         # line numbers are modified if range do not start at line 1
         if first > 1
-          @datastart = " data-start=\"#{first}\""
+          datastart = " data-start=\"#{first}\""
         end
       else
-        @code = File.read(@path)
+        code = File.read(path)
       end  
     else
-      raise SyntaxError.new("Syntax Error in 'codefile' - file #{@path} do not exists")
+      raise SyntaxError.new("Syntax Error in 'codefile' - file #{path} do not exists")
     end
-  end
 
-  def render(context)
     <<-HTML
 <div>
   <div class="code-toolbar">
     <div class="btn-group" role="group" aria-label="...">
-      <button type="button" class="btn btn-default"><a target="_blank" href="#{@url}"><i class="fa fa-file-text-o" aria-hidden="true"></i></a></button>
+      <button type="button" class="btn btn-default"><a target="_blank" href="#{url}"><i class="fa fa-file-text-o" aria-hidden="true"></i></a></button>
       <button type="button" class="btn btn-default btn-copy"><i class="fa fa-clipboard" aria-hidden="true"></i></button>
     </div>
   </div>
-  <pre class="language-#{@language}#{@linenumbers}"#{@highlight}#{@datastart}><code>#{@code}</code></pre>
+  <pre class="language-#{@language}#{@linenumbers}"#{@highlight}#{datastart}><code>#{code}</code></pre>
 </div>
       HTML
   end
