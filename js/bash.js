45b40876652df88d180a93baf597601e377e7759
arno-di-loreto <arno.di.loreto@gmail.com>
2020-02-23 16:04:10 +0100

    bash player: always visible progess bar not hiding content, play/pause button reacting to events

--- js/bash.js
+++ js/bash.js
@@ -6,18 +6,46 @@ const bashBootstrapCols = {
   xl: '125'
 }
 
-function getCodeBlockCard(element) {
-  return $(element).parents('.card-code')[0]
+function getBashPlayer(element) {
+  return $(element).parents('.card-bash').find('#player')
+}
+
+function getBashPlayerPlayPauseButton(element) {
+  return $(element).parents('.card-bash').find('#play')
+}
+
+function getBashPlayerPlayPauseButtonIcon(element) {
+  return getBashPlayerPlayPauseButton(element).find(':first-child')
+}
+
+function bashPlayerIsPlaying(element) {
+  return getBashPlayerPlayPauseButtonIcon(element).hasClass('fa-play')
+}
+
+function updateBashPlayerPlayPauseButtonIcon(element, event) {
+  const icon = getBashPlayerPlayPauseButtonIcon(element)
+  if(event.localeCompare('play') === 0){
+    console.log('play icon')
+    icon.removeClass('fa-pause')
+    icon.addClass('fa-play')
+  }
+  else {
+    console.log('pause icon')
+    icon.removeClass('fa-play')
+    icon.addClass('fa-pause')
+  }
 }
 
 function resizePlayers() {
   const size = getBootstrapDeviceSize()
   const expectedCols = bashBootstrapCols[size]
+  let resized = false
   $('.bash-player').each(function(index){
     const playerDiv = $(this)
-    const player = playerDiv.find('#player')
+    const player = getBashPlayer(playerDiv)
     const playerCols = player.attr('cols')
     if(playerCols !== expectedCols) {
+      resized = true
       const cols = expectedCols
       const rows = player.attr('rows')
       const title = player.attr('title')
@@ -31,17 +59,47 @@ function resizePlayers() {
       }
       const src = player.attr('src')
       const html = '<asciinema-player id="player" title="'+title+'" author="'+author+'" rows="'+rows+'" cols="'+cols+'" src="'+src+poster+'"></asciinema-player>'
+      const playerIsPlaying = bashPlayerIsPlaying(player)
+      const playerCurrentTime = player.currentTime
       playerDiv.html(html)
+      const newPlayer = getBashPlayer(playerDiv)
+      console.log(newPlayer)
+      newPlayer.currentTime = playerCurrentTime
+      updateBashPlayerPlayPauseButtonIcon(playerDiv, 'play')
+      addEventListenersToBashPlayer(newPlayer)
     }
-  });
+  })
+  return resized
 }
 
-
 function bashPlayPause(button) {
+  const player = getBashPlayer(button)
+  if(bashPlayerIsPlaying(player)){
+    player[0].play()
+  }
+  else {
+    player[0].pause()
+  }
+}
 
+function addEventListenersToBashPlayer(player) {
+  player[0].addEventListener('pause', function(e) {
+    updateBashPlayerPlayPauseButtonIcon(this, 'play')
+  })
+  player[0].addEventListener('play', function(e) {
+    updateBashPlayerPlayPauseButtonIcon(this, 'pause')
+  })
+}
+
+function addEventListenerToBashPlayer() {
+  $('.card-bash').find('#player').each(function(index) {
+    addEventListenersToBashPlayer(this)
+  })
 }
 
 $(document).ready(function() {
-  resizePlayers()
+  if(!resizePlayers()) {
+    addEventListenerToBashPlayer()
+  }
   $(window).on('resize', resizePlayers)
 })
