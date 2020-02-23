96a97c83d9f248d669d1affecea5de9a86a28b8c
arno-di-loreto <arno.di.loreto@gmail.com>
2020-02-23 21:16:00 +0100

    toc with experimental border highlight

--- js/toc.js
+++ js/toc.js
@@ -21,25 +21,72 @@ window.addEventListener('scroll', function(e) {
 });
 */
 
+function resetTocBorder() {
+  const activeItems = $('#toc .active')
+  if(activeItems.length === 0){
+    $('#toc .above-active').each(function(index){
+      $(this).removeClass('above-active')
+    })
+    $('#toc .below-active').each(function(index){
+      $(this).removeClass('below-active')
+    })
+  }
+}
+
+function updateTocBorder() {
+  $('#toc .above-active').each(function(index){
+    $(this).removeClass('above-active')
+  })
+  $('#toc .below-active').each(function(index){
+    $(this).removeClass('below-active')
+  })
+  $("#toc .active").each(function(index){
+    
+    let previous = $(this).prev()
+    if(previous.hasClass('nav')){
+      previous = previous.find('.nav-link:last-child')
+    }
+    if(!previous.hasClass('active')){
+      previous.addClass('above-active')
+    }
+
+    let next = $(this).next()
+    if(next[0] === undefined){
+      next = $(this).parent().next()
+    } else if(next.hasClass('nav')){
+      next = next.find('.nav-link:first-child')
+    }
+    if(!next.hasClass('active')){
+      next.addClass('below-active')
+    }
+
+  })
+}
+
 $(document).ready(function(){
   // Scroll TOC when needed in order to keep active element visible
   $(window).on('activate.bs.scrollspy', function () {
-      let activeItems = $("#toc .active")
-      let lastActiveItem = activeItems[activeItems.length-1]
-      // DIV around the #TOC
-      let toc = $("#scrollable-toc")[0]
-      let tocScroll = toc.scrollTop
-      let itemTopInsideToc = lastActiveItem.offsetTop
-      let itemBottomInsideToc = lastActiveItem.offsetTop + lastActiveItem.offsetHeight
-      tocVisibleTop = toc.scrollTop
-      tocVisibleBottom = tocScroll + toc.offsetHeight
-      if (itemTopInsideToc <= tocVisibleTop){
-        delta = itemTopInsideToc - tocVisibleTop
-        toc.scrollTop = toc.scrollTop + delta
-      }
-      if(itemBottomInsideToc >= tocVisibleBottom){
-        delta = itemBottomInsideToc - tocVisibleBottom
-        toc.scrollTop = toc.scrollTop + delta
-      }
+    updateTocBorder()
+    let activeItems = $("#toc .active")
+    let lastActiveItem = activeItems[activeItems.length-1]
+    // DIV around the #TOC
+    let toc = $("#scrollable-toc")[0]
+    let tocScroll = toc.scrollTop
+    let itemTopInsideToc = lastActiveItem.offsetTop
+    let itemBottomInsideToc = lastActiveItem.offsetTop + lastActiveItem.offsetHeight
+    tocVisibleTop = toc.scrollTop
+    tocVisibleBottom = tocScroll + toc.offsetHeight
+    if (itemTopInsideToc <= tocVisibleTop){
+      delta = itemTopInsideToc - tocVisibleTop
+      toc.scrollTop = toc.scrollTop + delta
+    }
+    if(itemBottomInsideToc >= tocVisibleBottom){
+      delta = itemBottomInsideToc - tocVisibleBottom
+      toc.scrollTop = toc.scrollTop + delta
+    }
   })
+
+  $(document).on('scroll', function(){
+    resetTocBorder()
+  });
 })
