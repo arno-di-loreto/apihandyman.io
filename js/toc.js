b7532e4c980f79512f1237452269b5ac2ae5d942
arno-di-loreto <arno.di.loreto@gmail.com>
2020-02-28 17:36:13 +0100

    left column flat

--- js/toc.js
+++ js/toc.js
@@ -24,24 +24,24 @@ window.addEventListener('scroll', function(e) {
 function resetTocBorder() {
   const activeItems = $('#toc .active')
   if(activeItems.length === 0){
-    $('#toc .above-active').each(function(index){
+    $('.left-column .above-active').each(function(index){
       $(this).removeClass('above-active')
     })
-    $('#toc .below-active').each(function(index){
+    $('.left-column .below-active').each(function(index){
       $(this).removeClass('below-active')
     })
   }
 }
 
 function updateTocBorder() {
-  $('#toc .above-active').each(function(index){
+  $('.left-column .above-active').each(function(index){
     $(this).removeClass('above-active')
   })
-  $('#toc .below-active').each(function(index){
+  $('.left-column .below-active').each(function(index){
     $(this).removeClass('below-active')
   })
   $("#toc .active").each(function(index){
-    
+    console.log($(this))
     let previous = $(this).prev()
     if(previous.hasClass('nav')){
       previous = previous.find('.nav-link:last-child')
@@ -59,8 +59,15 @@ function updateTocBorder() {
     if(!next.hasClass('active')){
       next.addClass('below-active')
     }
-
   })
+  const navLinks = $("#toc .nav-link")
+  if($(navLinks[0]).hasClass('active')){
+    $('.left-column').find('.toc-filler-top').addClass('above-active')
+  }
+  console.log($(navLinks[navLinks.length-1]))
+  if($(navLinks[navLinks.length-1]).hasClass('active')){
+    $('.left-column').find('.toc-filler-bottom').addClass('below-active')
+  }
 }
 
 $(document).ready(function(){
