fa867224f5e1f8e3356c624bdd38d2ed4d491174
arno-di-loreto <arno.di.loreto@gmail.com>
2020-03-17 18:23:10 +0100

    fixed toc scroll

--- js/toc.js
+++ js/toc.js
@@ -1,16 +1,3 @@
-
-function resetTocBorder() {
-  const activeItems = $('#toc .active')
-  if(activeItems.length === 0){
-    $('.left-column .above-active').each(function(index){
-      $(this).removeClass('above-active')
-    })
-    $('.left-column .below-active').each(function(index){
-      $(this).removeClass('below-active')
-    })
-  }
-}
-
 function toggleToc() {
   $('.left-column').toggleClass('left-column-shown')
 }
@@ -23,6 +10,34 @@ function toggleBackToTop() {
 $(document).ready(function(){
   // Scroll TOC when needed in order to keep active element visible
   $(window).on('activate.bs.scrollspy', function () {
+    const toc = $('#toc')
+    const tocScrollableParent = $('#toc')[0].parentElement
+    console.log('parent scrollTop', tocScrollableParent.scrollTop)
+    console.log('parent offsetTop', tocScrollableParent.offsetTop)
+    const activeLevel1 = $("#toc .toc-level-1.active")
+    if(activeLevel1.length > 0) {
+      console.log('level 1 active', activeLevel1[0].innerText)
+      console.log('level 1 active, offsetTop', activeLevel1[0].offsetTop)
+      console.log('level 1 active, offsetHeight', activeLevel1[0].offsetHeight)
+      const activeLevel2 = $("#toc .toc-level-2.active")
+      if(activeLevel2.length > 0) {
+        console.log('level 2 active', activeLevel2[0].innerText)
+        console.log('level 2 active, offsetTop', activeLevel2[0].offsetTop)
+        console.log('level 2 active, offsetHeight', activeLevel2[0].offsetHeight)
+        const level2Top =  activeLevel2[0].offsetTop - tocScrollableParent.offsetTop;
+        console.log('level 1 and level 2 active,  move to level2 top', level2Top)
+        tocScrollableParent.scrollTop = level2Top
+      }
+      else {
+        const level1Top = activeLevel1[0].offsetTop - tocScrollableParent.offsetTop;
+        console.log('only level 1 active, move to', level1Top)
+        // Move level 1 to top
+        tocScrollableParent.scrollTop = level1Top
+      }
+      console.log('parent updated scrollTop', tocScrollableParent.scrollTop)
+    }
+
+    /*
     let activeItems = $("#toc .active")
     let lastActiveItem = activeItems[activeItems.length-1]
     // DIV around the #TOC
@@ -40,6 +55,7 @@ $(document).ready(function(){
       delta = itemBottomInsideToc - tocVisibleBottom
       toc.scrollTop = toc.scrollTop + delta
     }
+    */
   })
 
   $('.left-column-toggler').on('click', function(){
