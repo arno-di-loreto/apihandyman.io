e8a08244b42eed7284c2dfb02baa6dea99556700
arno-di-loreto <arno.di.loreto@gmail.com>
2021-06-19 15:41:06 +0000

    Publish

--- _site/code/api-toolbox-jq-and-openapi/part-2/list-operations.jq
+++
@@ -1,4 +0,0 @@
-include "module-openapi"; # Imports module-openapi.jq file
-
-oas_operations | # Function coming from module-openapi.jq file
-oas_operations_to_text  # Function coming from module-openapi.jq file
--- _site/js/anchors.js
+++
@@ -1,6 +0,0 @@
-anchors.options = {
-  placement: 'right',
-  visible: 'always',
-  icon: '#'
-};
-anchors.add('.anchors .page-section-not-title h1, .anchors .page-section-not-title h2, .anchors .page-section-not-title h3');
